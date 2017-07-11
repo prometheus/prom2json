a82e3aba129ed753c8e70f947627b48b048d6850
Christian Kniep <christian@qnib.org>
2017-07-11 17:52:55 +0200

    add ToOpenTSDBv1() method and test

--- metric.go
+++ metric.go
@@ -5,12 +5,18 @@ import (
 	"io"
 	"mime"
 	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/matttproud/golang_protobuf_extensions/pbutil"
 	"github.com/prometheus/common/expfmt"
 	"github.com/prometheus/log"
 
+	"errors"
 	dto "github.com/prometheus/client_model/go"
+	"regexp"
 )
 
 const acceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,text/plain;version=0.0.4;q=0.3`
@@ -18,16 +24,26 @@ const acceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client
 // Family mirrors the MetricFamily proto message.
 type Family struct {
 	//Time    time.Time
-	Name    string        `json:"name"`
-	Help    string        `json:"help"`
-	Type    string        `json:"type"`
-	Metrics []interface{} `json:"metrics,omitempty"` // Either metric or summary.
+	Name    string    `json:"name"`
+	Help    string    `json:"help"`
+	Type    string    `json:"type"`
+	Metrics []Metrics `json:"metrics,omitempty"` // Either metric or summary.
+}
+
+// Metrics ensures that all metric types implement common functions like GetLabels().
+type Metrics interface {
+	GetLabels() map[string]string
 }
 
 // Metric is for all "single value" metrics, i.e. Counter, Gauge, and Untyped.
 type Metric struct {
 	Labels map[string]string `json:"labels,omitempty"`
 	Value  string            `json:"value"`
+}
+
+// GetLabels returns the Labels of the Metric type to implement the Metrics interface.
+func (m Metric) GetLabels() map[string]string {
+	return m.Labels
 }
 
 // Summary mirrors the Summary proto message.
@@ -38,12 +54,22 @@ type Summary struct {
 	Sum       string            `json:"sum"`
 }
 
+// GetLabels returns the Labels of the Summary type to implement the Metrics interface.
+func (s Summary) GetLabels() map[string]string {
+	return s.Labels
+}
+
 // Histogram mirrors the Histogram proto message.
 type Histogram struct {
 	Labels  map[string]string `json:"labels,omitempty"`
 	Buckets map[string]string `json:"buckets,omitempty"`
 	Count   string            `json:"count"`
 	Sum     string            `json:"sum"`
+}
+
+// GetLabels returns the Labels of the Histogram type to implement the Metrics interface.
+func (h Histogram) GetLabels() map[string]string {
+	return h.Labels
 }
 
 // NewFamily consumes a MetricFamily and transforms it to the local Family type.
@@ -53,7 +79,7 @@ func NewFamily(dtoMF *dto.MetricFamily) *Family {
 		Name:    dtoMF.GetName(),
 		Help:    dtoMF.GetHelp(),
 		Type:    dtoMF.GetType().String(),
-		Metrics: make([]interface{}, len(dtoMF.Metric)),
+		Metrics: make([]Metrics, len(dtoMF.Metric)),
 	}
 	for i, m := range dtoMF.Metric {
 		if dtoMF.GetType() == dto.MetricType_SUMMARY {
@@ -181,4 +207,66 @@ func (f *Family) AddLabel(key, val string) {
 			f.Metrics[i] = m
 		}
 	}
+}
+
+// ToOpenTSDBv1 transforms the metrics(not yet Histograms/Summaries) to OpenTSDB line format (v1),
+// returns an array of lines.
+func (f *Family) ToOpenTSDBv1() []string {
+	base := fmt.Sprintf("put %s %d", f.Name, time.Now().Unix())
+	res := []string{}
+	for _, item := range f.Metrics {
+		switch item.(type) {
+		case Metric:
+			m := item.(Metric)
+			val, err := strconv.ParseFloat(m.Value, 64)
+			if err != nil {
+				continue
+			}
+			lab, err := LabelToString(m.Labels)
+			if err != nil {
+				met := fmt.Sprintf("%s %f", base, val)
+				res = append(res, met)
+			} else {
+				met := fmt.Sprintf("%s %f %s", base, val, strings.Join(lab, " "))
+				res = append(res, met)
+			}
+		default:
+			log.Printf("Type '%s' not yet implemented", reflect.TypeOf(item))
+		}
+	}
+	return res
+}
+
+// LabelToString consumes a k/v map and returns a sanitized []string{} with key=val pairs.
+// In case the map is empty or all k/v pairs fail the sanitization test, an error is return.
+func LabelToString(inp map[string]string) (lab []string, err error) {
+	if len(inp) == 0 {
+		return nil, errors.New("amp is empty, therefore no string for you")
+	}
+	for k, v := range inp {
+		tag, err := SanitizeTags(k, v)
+		if err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+		lab = append(lab, tag)
+	}
+	if len(lab) == 0 {
+		return nil, errors.New("all k/v pairs failed the sanitization test")
+	}
+	return
+}
+
+// SanitizeTags checks whether the tag is compliant with the rules defined in
+// http://opentsdb.net/docs/build/html/user_guide/writing.html#metrics-and-tags.
+// For starters only `^[a-zA-Z0-9\-\./]+$` is allowed.
+// TODO: Add Unicode letters
+func SanitizeTags(k, v string) (tag string, err error) {
+	// a to z, A to Z, 0 to 9, -, _, ., /
+	r := regexp.MustCompile(`^[a-zA-Z0-9\-\./]+$`)
+	if !r.MatchString(k) || !r.MatchString(v) {
+		return "", fmt.Errorf(`Did not match '[a-zA-Z\-\./]+': %s=%s`, k, v)
+	}
+	tag = fmt.Sprintf("%s=%s", k, v)
+	return
 }

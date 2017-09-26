69998802d07dac5fa9f7eed5ab638e2eb551efa3
樊尚享 <martinfan@yunify.com>
2017-09-26 19:07:04 +0800

    rename option name

--- prom2json.go
+++ prom2json.go
@@ -124,7 +124,7 @@ func makeBuckets(m *dto.Metric) map[string]string {
 func FetchMetricFamilies(
 	url string, ch chan<- *dto.MetricFamily,
 	certificate string, key string,
-	skipCACheck bool,
+	skipServerCertCheck bool,
 ) {
 	defer close(ch)
 	var transport *http.Transport
@@ -138,12 +138,15 @@ func FetchMetricFamilies(
 		}
 		tlsConfig.BuildNameToCertificate()
 		transport = &http.Transport{TLSClientConfig: tlsConfig}
-	} else if skipCACheck {
-		transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
 	} else {
 		transport = &http.Transport{}
+	}
+	if skipServerCertCheck {
+		if transport.TLSClientConfig != nil {
+			transport.TLSClientConfig.InsecureSkipVerify = true
+		} else {
+			transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		}
 	}
 	client := &http.Client{Transport: transport}
 	decodeContent(client, url, ch)

e42ab5c27f95dd1e328fd878d96fde5f47f6a07c
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2018-11-20 17:10:07 +0100

    allow reading from stdin and file (#35)
    Allow reading from either of: url, file or stdin
    
    Signed-off-by: Krzysztof Nazarewski <nazarewk@gmail.com>

--- cmd/prom2json/main.go
+++ cmd/prom2json/main.go
@@ -17,6 +17,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/url"
 	"os"
 
 	"github.com/prometheus/common/log"
@@ -26,37 +28,66 @@ import (
 	"github.com/prometheus/prom2json"
 )
 
+var USAGE = fmt.Sprintf(`Usage: %s [METRICS_PATH | METRICS_URL [--cert CERT_PATH --key KEY_PATH | --accept-invalid-cert]]`, os.Args[0])
+
 func main() {
-	cert := flag.String("cert", "", "certificate file")
-	key := flag.String("key", "", "key file")
+	cert := flag.String("cert", "", "client certificate file")
+	key := flag.String("key", "", "client certificate's key file")
 	skipServerCertCheck := flag.Bool("accept-invalid-cert", false, "Accept any certificate during TLS handshake. Insecure, use only for testing.")
 	flag.Parse()
 
-	if len(flag.Args()) != 1 {
-		log.Fatalf("Usage: %s METRICS_URL", os.Args[0])
+	var input io.Reader
+	var err error
+	arg := flag.Arg(0)
+	flag.NArg()
+
+	if flag.NArg() > 1 {
+		log.Fatalf("Too many arguments.\n%s", USAGE)
 	}
-	if (*cert != "" && *key == "") || (*cert == "" && *key != "") {
-		log.Fatalf("Usage: %s METRICS_URL\n with TLS client authentication: %s -cert=/path/to/certificate -key=/path/to/key METRICS_URL", os.Args[0], os.Args[0])
+
+	if arg == "" {
+		// Use stdin on empty argument
+		input = os.Stdin
+	} else if URL, urlErr := url.Parse(arg); urlErr != nil || URL.Scheme == "" {
+		// Note: `URL, err := url.Parse("/some/path.txt")` results in: `err == nil && URL.Scheme == ""`
+		// Open file since arg appears not to be a valid url (parsing error occurred or the scheme is missing)
+		if input, err = os.Open(arg); err != nil {
+			log.Fatal("error opening file:", err)
+		}
+	} else {
+		// validate Client SSL arguments since arg appears to be a valid URL
+		if (*cert != "" && *key == "") || (*cert == "" && *key != "") {
+			log.Fatalf("%s\n with TLS client authentication: %s --cert /path/to/certificate --key /path/to/key METRICS_URL", USAGE, os.Args[0])
+		}
 	}
 
 	mfChan := make(chan *dto.MetricFamily, 1024)
 
-	go func() {
-		err := prom2json.FetchMetricFamilies(flag.Args()[0], mfChan, *cert, *key, *skipServerCertCheck)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	// missing input means we are reading from an URL
+	if input != nil {
+		go func() {
+			if err := prom2json.ParseReader(input, mfChan); err != nil {
+				log.Fatal("error reading metrics:", err)
+			}
+		}()
+	} else {
+		go func() {
+			err := prom2json.FetchMetricFamilies(arg, mfChan, *cert, *key, *skipServerCertCheck)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}()
+	}
 
 	result := []*prom2json.Family{}
 	for mf := range mfChan {
 		result = append(result, prom2json.NewFamily(mf))
 	}
-	json, err := json.Marshal(result)
+	jsonText, err := json.Marshal(result)
 	if err != nil {
 		log.Fatalln("error marshaling JSON:", err)
 	}
-	if _, err := os.Stdout.Write(json); err != nil {
+	if _, err := os.Stdout.Write(jsonText); err != nil {
 		log.Fatalln("error writing to stdout:", err)
 	}
 	fmt.Println()
